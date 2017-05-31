51592e58e05c91ee53c3dd05c06875eb9cd7af47
Juraj Pelikan <juraj.pelikan@gmail.com>
2017-05-31 20:33:26 +0200

    New keybindings
    - chords expand-region
    - helm-git-grep

--- personal/orion-editing.el
+++ personal/orion-editing.el
@@ -1,6 +1,6 @@
 ;; Editing modifications
 ;; Code
-(prelude-require-packages '(shackle yasnippet goto-last-change))
+(prelude-require-packages '(shackle yasnippet goto-last-change helm-git-grep))
 
 (setq shackle-rules '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :ratio 0.46)))
 (shackle-mode)
@@ -16,6 +16,8 @@
   "comment or uncomment current line or region"
   (interactive)
   (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+
+(setq company-dabbrev-downcase nil)
 
 (provide 'orion-editing)
 ;;; orion-editing.el ends here
--- personal/orion-keybindings.el
+++ personal/orion-keybindings.el
@@ -1,7 +1,7 @@
 ;; Orion keybindings
 
-(key-chord-define-global "gg" 'avy-goto-line)
-(key-chord-define-global ".." 'er/expand-region)
+;; (key-chord-define-global "gg" 'avy-goto-line)
+(key-chord-define-global ",," 'er/expand-region)
 
 (eval-after-load "flyspell"
   '(define-key flyspell-mode-map (kbd "C-.") nil))
@@ -9,6 +9,9 @@
 (global-set-key (kbd "C-.") 'er/mark-symbol)
 
 (global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+(define-key prelude-mode-map (kbd "C-c g") nil)
+(global-set-key (kbd "C-c g") 'helm-git-grep)
 
 (provide 'orion-keybindings)
 ;; orion-python.el ends here
